9a2b51f33d34ebbd0bf793f486e7159d1b33cf7c
aclark <aclark@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-01-26 09:02:27 +0000

    Moving trunk to egg (rise, dead product, rise! ;-)

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.6.0'
+
+setup(name='Products.EventRegistration',
+      version=version,
+      description="an event registration system for plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='plone events',
+      author='Jason McVetta',
+      author_email='jmcvetta@squaretortoise.com',
+      url='http://svn.plone.org/svn/collective/Products.EventRegistration/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [distutils.setup_keywords]
+      paster_plugins = setuptools.dist:assert_string_list
+
+      [egg_info.writers]
+      paster_plugins.txt = setuptools.command.egg_info:write_arg
+      """,
+      paster_plugins = ["ZopeSkel"],
+      )
