5c730a9af186b092fde605dfea0ea7d91788ff38
Alex Clark <aclark@aclark.net>
2012-11-05 18:16:49 -0500

    Clean up

--- setup.py
+++ setup.py
@@ -31,12 +31,7 @@ setup(name='Products.EventRegistration',
       ],
       entry_points="""
       # -*- Entry points: -*-
-
-      [distutils.setup_keywords]
-      paster_plugins = setuptools.dist:assert_string_list
-
-      [egg_info.writers]
-      paster_plugins.txt = setuptools.command.egg_info:write_arg
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
-      paster_plugins = ["ZopeSkel"],
       )
